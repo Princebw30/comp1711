d662eb2ecc0793697073dd1df5cdb628d7d614ee
Brian Wong <146480210+Princebw30@users.noreply.github.com>
2023-11-16 17:48:23 +0000

    task2

--- sem1 coursework/stepcounterv2.c
+++ sem1 coursework/stepcounterv2.c
@@ -8,16 +8,28 @@
 int main() {
     char option;
     int check = 0;
+    
     int buffer_size = 500;
-    char line_buffer[500]; 
+    char line_buffer[100]; 
+    //char line_buffer[500]; 
     FITNESS_DATA fitness_data1;
     FILE *file = NULL; 
 
     while (check == 0) {
-        printf("Enter option: ");
+        printf("Menu options: \n");
+        printf("A: Specify the filename to be imported\n");
+        printf("B: Dislay the total number of records in the file\n");
+        printf("C: Find the date and time of the timeslot with the fewest steps\n");
+        printf("D: Find the date andtime of the timeslot with the largest number of steps\n");
+        printf("E: Find te mean step count of all the records in the file\n");
+        printf("F: Find the longest continuous period where the step count is above 500 steps\n");
+        printf("Q: Quit\n");
+        printf("Enter choice: ");
         scanf(" %c", &option); 
 
         switch (option) {
+
+            //case A
             case 'A':
                 printf("Input filename: ");
                 char filename[100];
@@ -27,8 +39,10 @@ int main() {
                     printf("Error: Could not open file\n");
                     exit(1);
                 }
+                printf("File successfully loaded.");
                 break;
 
+            //case B
             case 'B':
                 if (file == NULL) 
                 {
@@ -45,6 +59,7 @@ int main() {
                 rewind(file);
                 break;
 
+            // //case C
             case 'C':
             if (file == NULL) 
             {
@@ -74,6 +89,7 @@ int main() {
             rewind(file);
             break;
 
+            // //case D
             case 'D':
             if (file == NULL) 
             {
@@ -103,6 +119,8 @@ int main() {
             }
             rewind(file);
             break;
+
+            // //case E
             case 'E':
             if (file == NULL) 
             {
@@ -126,10 +144,83 @@ int main() {
             }
             rewind(file);
             break;
+
+            // //case F
             case 'F':
+            if (file == NULL)
+            {
+                printf("Error: File not opened\n");
+            }
+            else 
+            {
+                char startDate[11];
+                char startDate1[11];
+                char endDate[11];
+                char endDate1[11];
+                char startTime[7];
+                char startTime1[7];
+                char endTime[7];
+                char endTime1[7];
+                int periodCount = 0;
+                int periodCount1 = 0;
+                int check = 0;
+
+                FITNESS_DATA fitness_data2;
+
+                while(fgets(line_buffer, buffer_size, file) != NULL)
+                {
+                    tokeniseRecord(line_buffer, ",", fitness_data2.date, fitness_data2.time, fitness_data2.steps);
+                    if ( check == 0)
+                    {
+                        if (atoi(fitness_data2.steps) > 500)
+                        {
+                            strcpy( startDate1 , fitness_data2.date);
+                            strcpy( startTime1 , fitness_data2.time);
+                            strcpy( endDate1 , fitness_data2.date);
+                            strcpy(endTime1 , fitness_data2.time);
+                            check = 1;
+                            periodCount1 = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (atoi(fitness_data2.steps) > 500)
+                        {
+                            strcpy( endDate1 , fitness_data2.date);
+                            strcpy( endTime1 , fitness_data2.time);
+                            periodCount1++;
+                        }
+                        else
+                        {
+                            if (periodCount1 > periodCount)
+                            {
+                                strcpy( startDate , startDate1);
+                                strcpy( startTime , startTime1);
+                                strcpy( endDate , endDate1);
+                                strcpy( endTime , endTime1);
+                                periodCount = periodCount1;
+                            }
+                            periodCount1 = 0;
+                            check = 0;
+                        }
+                    }
+                }
+                if (periodCount1 > periodCount)
+                {
+                    strcpy( startDate , startDate1);
+                    strcpy( startTime , startTime1);
+                    strcpy( endDate , endDate1);
+                    strcpy( endTime , endTime1);
+                    periodCount = periodCount1;
+                }
+                printf("Longest period start: %s %s\n" , startDate , startTime);
+                printf("Longest period end: %s %s\n" , endDate , endTime);
+                
+            }
             rewind(file);
             break;
 
+            // //case Q
             case 'Q':
                 check = 1;
                 printf("Exit\n");
@@ -139,8 +230,9 @@ int main() {
                 }
                 break;
 
+            //default
             default:
-                printf("Invalid option: %c\n", option);
+                printf("Invalid choice. Try again");
         }
     }
 
